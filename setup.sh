7502c83b1be4dfd2089dce3ab98791ba242ab951
Youssef <youssef95elzein@gmail.com>
2025-01-16 10:28:45 -0800

    Added moon trek demo

--- setup.sh
+++ setup.sh
@@ -1,20 +1,29 @@
 sudo apt update && sudo apt upgrade
-sudo apt install ufw nodejs npm nginx certbot python3-certbot-nginx
-
-cd server && npm install
-cd ../client && npm install && npm run build
-
-sudo mkdir -p /var/www/youssefelzein.com/html
-sudo chmod -R 755 /var/www/youssefelzein.com
-sudo cp -r dist/* /var/www/youssefelzein.com/html/
-
-cd ../
+sudo apt install ufw nodejs npm nginx certbot python3-certbot-nginx docker.io
 
 sudo cp youssefelzein.com /etc/nginx/sites-available/youssefelzein.com
 sudo ln -s /etc/nginx/sites-available/youssefelzein.com /etc/nginx/sites-enabled/
 
 sudo cp start-server.service /etc/systemd/system/
+sudo cp start-moon-trek-server.service /etc/systemd/system/
+
+sudo mkdir -p /var/www/youssefelzein.com/moon-trek/html
+sudo mkdir /var/www/youssefelzein.com/html
+sudo chmod -R 755 /var/www/youssefelzein.com
+
+cd server && npm install
+cd ../client && npm install && npm run build
+sudo cp -r dist/* /var/www/youssefelzein.com/html/
+
+git clone https://github.com/youssefe95/moon-trek-jpl-horizon ~/moon-trek-jpl-horizon
+cd ~/moon-trek-jpl-horizon/server && npm install
+cd ../client
+sed -i "s|http://localhost:8890|https://youssefelzein.com|g" src/App.vue
+npm install && npm run build
+sudo cp -r dist/* /var/www/youssefelzein.com/moon-trek/html/
+
 sudo systemctl enable start-server.service
+sudo systemctl enable start-moon-trek-server.service
 
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
--- youssefelzein.com
+++ youssefelzein.com
@@ -2,18 +2,29 @@ server {
     listen 80;
     listen [::]:80;
 
+    client_max_body_size 8M;
+
     root /var/www/youssefelzein.com/html;
 
     error_page 404 /;
-    index index.html index.htm index.nginx-debian.html;
+    index index.html;
 
     server_name youssefelzein.com www.youssefelzein.com;
 
     location / {
-            try_files $uri $uri/ =404;
+        try_files $uri $uri/ =404;
     }
 
     location /static {
         proxy_pass http://localhost:8888;
+    }
+
+    location /moon-trek {
+        proxy_pass http://localhost:8890;
+    }
+
+    location /moon-trek/demo {
+        alias /var/www/youssefelzein.com/moon-trek/html;
+        try_files $uri $uri/ =404;
     }
 }
